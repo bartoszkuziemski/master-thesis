05ecfb8e2692e138a277e7b9cbaac0de907c4cb7
BartoszKuziemski <bartuspl99@gmail.com>
2023-04-27 23:17:10 +0200

    native images builds

--- master-thesis-quarkus/src/main/java/com/example/dto/ProductDto.java
+++ master-thesis-quarkus/src/main/java/com/example/dto/ProductDto.java
@@ -4,7 +4,9 @@ package com.example.dto;
 import com.example.model.enums.CategoryType;
 import com.example.model.enums.ConditionType;
 import com.example.model.enums.OfferType;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
+@RegisterForReflection
 public record ProductDto(
         Long id,
         String name,
