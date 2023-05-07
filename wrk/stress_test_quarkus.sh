00763881e794589529c80e009c7751e109e2cfb7
BartoszKuziemski <bartuspl99@gmail.com>
2023-05-07 20:35:23 +0200

    removed cpu limit

--- wrk/stress_test_quarkus.sh
+++ wrk/stress_test_quarkus.sh
@@ -5,16 +5,13 @@ function cleanup() {
     exit 1
 }
 
-command="./wrk -t1 -c50 -d5m -R500 -s ./lua/get_product_by_id.lua --latency http://localhost:8081"
-$command
-
 echo "stress_test" >> results.csv
 
-for i in {1..20}
+for i in {1..30}
 do
     rate=$((i * 100))
     connections=$((i * 10))
-    time=3
+    time=5
     command="./wrk -t1 -c$connections -d${time}m -R$rate -s ./lua/all_requests.lua --latency http://localhost:8081"
     echo -n "$time,$connections,$rate," >> results.csv
     $command || cleanup
---
+++ wrk/stress_test_spring.sh
@@ -0,0 +1,18 @@
+trap "cleanup" SIGINT
+
+function cleanup() {
+    echo "Script terminated." >&2
+    exit 1
+}
+
+echo "stress_test" >> results.csv
+
+for i in {1..18}
+do
+    rate=$((i * 100))
+    connections=$((i * 10))
+    time=5
+    command="./wrk -t1 -c$connections -d${time}m -R$rate -s ./lua/all_requests.lua --latency http://localhost:8081"
+    echo -n "$time,$connections,$rate," >> results.csv
+    $command || cleanup
+done
